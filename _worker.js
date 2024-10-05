61182436b0af063f0496b25f8564b7e7cbf0ca13
0306dududu <106128939+0306dududu@users.noreply.github.com>
2024-10-05 17:57:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '318fb44d-6b9e-4dc1-a0aa-67767e931a0a';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
